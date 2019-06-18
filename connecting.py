1463884fbe7ae600b989408a456a949ab10c6013
Sanchit khera <43273507+Sanchit987@users.noreply.github.com>
2019-06-18 14:38:10 +0530

    update connecting.py
    i have added comments to make better understanding of the script.

--- connecting.py
+++ connecting.py
@@ -1,12 +1,13 @@
+# Note- some email such as gmail we need to set the settings to less secure only then we can use this script to send mails 
 import smtplib
-file1 = open(r"C:\Users\WIN 10\Desktop\email.txt","r")
+file1 = open(r"C:\Users\WIN 10\Desktop\email.txt","r")    #location of text file 
 a = file1.readlines()
 b = []
 for email in a:
     b.append(email.strip("\n"))
 print(b)
-sender = "email"
-passcode = "password here"
+sender = "email"             #your email    
+passcode = "password here"   #your password
 for i in range(0,len(b)):
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
